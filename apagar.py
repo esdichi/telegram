7f2ee248f5efeff5c38c6191c6f7ad8a2338889b
esdichi <163755759+esdichi@users.noreply.github.com>
2026-03-02 16:13:13 +0100

    Update apagar.py

--- apagar.py
+++ apagar.py
@@ -1,26 +1,15 @@
-import os
+import subprocess
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TELEGRAM_TOKEN = "TU TOKEN_AQUI"
+TOKEN = "TU API KEY"
 
-USUARIOS_AUTORIZADOS = [12321321312] 
-
-async def apagar_pc(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    usuario_id = update.effective_user.id
-    if usuario_id not in USUARIOS_AUTORIZADOS:
-        await update.message.reply_text("No estás autorizado para apagar el PC.")
-        return
-
-    await update.message.reply_text("Apagando el PC en 10 segundos...")
-    print("Comando de apagado recibido desde Telegram.")
+async def apagar(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Confirmado. Apagando el sistema... ⚡")
     
-    os.system("notepad")  # /s = apagar, /t 10 = en 10 segundos
+    subprocess.run(["shutdown", "/s", "/t", "1"])
 
-
-app = Application.builder().token(TELEGRAM_TOKEN).build()
-
-app.add_handler(CommandHandler("apagar", apagar_pc))
-
-print("Bot de Telegram iniciado...")
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("apagar", apagar))
 app.run_polling()
+
